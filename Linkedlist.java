54e822a41972e2d27c6a347df0b8d91024a6fe4e
sun1235 <work.tanvo@gmail.com>
2016-05-05 20:59:24 -0500

    Added files via upload

--- Linkedlist.java
+++ Linkedlist.java
@@ -25,7 +25,7 @@ public class Linkedlist {
 
     public void print(String search) {
         int counter = 0;
-        System.out.println(" There are " + print(root, search, counter) + " people work in " + search);
+        System.out.println("There are " + print(root, search, counter) + " people work in " + search);
 
     }
 
@@ -36,11 +36,51 @@ public class Linkedlist {
 
         if (root.name.equals(search)) {
             counter++;
+            System.out.println(root.right.name);
         }
         counter = print(root.left, search, counter);
         counter = print(root.right, search, counter);
 
         return counter;
     }
+
+//#######################Does Not Work #############################3333
+//    public void Sort() {
+////        int counter = 0;
+////        System.out.println("There are " + print(root, search, counter) + " people work in " + search);
+//        Sort(root);
+//
+//    }
+//
+//    private void Sort (Node root) {
+//        String nameTemp;
+//        String departmentTemp;
+//        if (root == null) {
+//            return;
+//        }
+//
+//        if (root.right.name.toString().compareTo(root.left.right.name.toString()) > 0) {
+//            nameTemp = root.right.name.toString();
+//            root.right.name = root.left.right.name;
+//            root.left.right.name = nameTemp;
+//
+//            departmentTemp = root.name.toString();
+//            root.name = root.left.name;
+//            root.left.name = departmentTemp;
+//
+//            System.out.println("Name: " + root.right.name + "\t \t Department: " + root.name);
+//
+//            Sort(root.left);
+//
+//
+//        }
+//
+////        if (root.right != null) {
+////            System.out.println("Name: " + root.right.name + "\t \t Department: " + root.name);
+////        }
+//        Sort(root.left);
+////        Sort(root.right);
+//    }
 }
+
 
--- Main.java
+++ Main.java
@@ -14,17 +14,32 @@ public class Main {
         FileReader fr = new FileReader("/home/gravitational/Documents/Project3/src/employee.txt");
         BufferedReader br = new BufferedReader(fr);
         String line;
-
+        int position = 0;
+        String buffer = " ";
         while ((line = br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(line);
             while (st.hasMoreTokens()) {
+                if (position == 0)
+                    {
+                        String tokenEmployee = st.nextToken();
+                        buffer = tokenEmployee;
+                        position++;
+                    }
+                else
+                {
                     String tokenEmployee = st.nextToken();
                     name.insert(tokenEmployee);
+                    name.insert(buffer);
+                    position = 0;
+                }
             }
         }
 
-        System.out.println("Please enter the department you want to look for: "  );
+        System.out.print("Please enter the department you want to look for: "  );
         String department = input.nextLine();
+        System.out.println();
         name.print(department);
+        System.out.println();
+        name.Sort();
     }
 }
